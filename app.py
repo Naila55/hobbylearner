6fba5f11017b12a24da8046096cade063021099f
Naila55 <f20230106@dubai.bits-pilani.ac.in>
2025-06-20 10:46:42 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index():
         uploaded_file = request.files["raw_file"]
         filename = uploaded_file.filename
         raw_email =  uploaded_file.read()
-        verdict, auth, frm, rply = email_analyzer.analyze_email(raw_email)
+        verdict, auth, frm, rply, rtnpath = email_analyzer.analyze_email(raw_email)
+
         print("File uploaded:", uploaded_file.filename)
 
         result = {
@@ -20,10 +21,11 @@ def index():
             "dkim": auth["DKIM"],
             "dmarc": auth["DMARC"],
             "from": frm,
-            "reply_to": rply
+            "reply_to": rply,
+            "return_path": rtnpath
         }
     return render_template("index.html", result=result, filename=filename)
 
 
-#if __name__ == "__main__":
-app.run(debug=True)          # http://127.0.0.1:5000
+
+app.run(port=5600,host="0.0.0.0")
